aedb5060e95fa8cd08ab67e2f75b1f27d9809566
miss-linetta <natashapetrovych@gmail.com>
2026-05-13 11:24:55 +0300

    Initial commit

---
+++ AquaTrail/AquaTrail/AquaTrailApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaTrailApp.swift
+//  AquaTrail
+//
+//  Created by admin on 06.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct AquaTrailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
